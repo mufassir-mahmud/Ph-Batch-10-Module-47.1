ba768f2d1fcf033e05878e3d0df0f2d524341baa
Mufassir Mahmud <mufassir389@gmail.com>
2025-09-14 11:53:59 +0600

    first commit

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='text-center'>
+            <h2 className='text-3xl'>Page Not Found</h2>
+            <p className='font-bold text-2xl'>404</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
