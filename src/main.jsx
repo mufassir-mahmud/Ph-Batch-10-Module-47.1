46214fc470f6adf9f71253f7aaa0475e0f418df1
Mufassir Mahmud <mufassir389@gmail.com>
2025-09-14 12:13:52 +0600

    second comment

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import Root from './components/Root/Root.jsx';
 import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
+import Home from './components/Home/Home.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,7 +16,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage/>,
     children: [
       {
-        
+        path: "/",
+        element: <Home/>,
       }
     ]
     
