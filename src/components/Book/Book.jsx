30aeb4559945b6c0c55ad8d9000fe8b7d14b4525
Mufassir Mahmud <mufassir389@gmail.com>
2025-09-14 14:04:56 +0600

    Added Book

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Book = ({book}) => {
+    const {bookName,author,image,tags} = book;
+    return (
+        <div>
+            <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img className='h-[166px] p-4'
+      src={image}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <div className="card-actions">
+      <div className="badge badge-outline">{tags[0]}</div>
+      <div className="badge badge-outline">{tags[1]}</div>
+    </div>
+    <h2 className="card-title">
+      {bookName}
+      <div className="badge badge-secondary">NEW</div>
+    </h2>
+    <p>{author}</p>
+    
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Book;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Books from '../Books/Books';
 
 const Home = () => {
     return (
         <div>
-            <h2 className='text-2xl text-center'>This is Home</h2>
+            <Banner/>
+            <Books/>
         </div>
     );
 };
